a858607de3b0a24b3d5bc3130e14ecc9ae40130c
Mike Merritt <emmjemm@gmail.com>
2012-05-03 15:47:15 -0500

    element positioning

--- js/jquery.tinyTips.js
+++ js/jquery.tinyTips.js
@@ -1,18 +1,35 @@
-﻿/***********************************************************/
+/***********************************************************/
 /*                    TinyTips Plugin                      */
 /*                      Version: 1.3                       */
 /*                      Mike Merritt                       */
-/*        https://github.com/mikemerritt/TinyTips          */
+/*         https://github.com/mikemerritt/TinyTips         */
 /***********************************************************/
 
 (function($) {
-	$.fn.tinyTips = function(options) {
-		
-		var defaults = {
-			content: 'title'
-		};
-		
-		var options = $.extend(defaults, options);
-		
-	
+  $.fn.tinyTips = function(options) {
+    
+    var defaults = {
+      content: 'title'
+    };
+    
+    var options = $.extend(defaults, options);
+    var markup = '<div id="tinytip"></div>';
+    $('body').append(markup);
+    $('#tinytip').hide();
+
+    $(this).on("mouseover", function() {
+
+      var pos = $(this).position();
+
+      var target = {
+        y: pos.top,
+        x: pos.left,
+        w: $(this).outerWidth(),
+        h: $(this).outerHeight()
+      };
+
+      console.log(target);
+
+    });
+  }
 })(jQuery);
