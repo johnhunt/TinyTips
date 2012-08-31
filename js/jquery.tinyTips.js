c7eb1a3fe2664916e70d10b730ff718d813b4f35
Mike <emmjemm@gmail.com>
2012-08-30 19:43:18 -0500

    fixes

--- js/jquery.tinyTips.js
+++ js/jquery.tinyTips.js
@@ -2,17 +2,45 @@
 /*                    TinyTips Plugin                      */
 /*                      Version: 1.3                       */
 /*                      Mike Merritt                       */
-/*        https://github.com/mikemerritt/TinyTips          */
+/*         https://github.com/mikemerritt/TinyTips         */
 /***********************************************************/
 
 (function($) {
-	$.fn.tinyTips = function(options) {
-		
-		var defaults = {
-			content: 'title'
-		};
-		
-		var options = $.extend(defaults, options);
-		
-	
+  $.fn.tinyTips = function(options) {
+    
+    var defaults = {
+      content: 'title',
+      position: 'top'
+    };
+    
+    var options = $.extend(defaults, options);
+    var markup = '<div id="tinytip"></div>';
+    $('body').append(markup);
+    $('#tinytip').hide();
+
+    $(this).on("mouseover", function() {
+
+      console.log($(this).attr('tt'));
+
+      $('#tinytip').append($(this).attr('tt'));
+
+      var pos = $(this).position();
+
+      var target = {
+        y: pos.top,
+        x: pos.left,
+        w: $(this).outerWidth(),
+        h: $(this).outerHeight(),
+        c: $(this).outerWidth()/2
+      };
+
+      var tooltip = {
+        top: {
+          y: target.y-20,
+          x: target.x
+        }
+      };
+
+    });
+  }
 })(jQuery);
