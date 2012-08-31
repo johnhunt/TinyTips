b187439cee6f1b2465385a3c329bc2c4cfd7e0a2
Mike <emmjemm@gmail.com>
2012-08-30 21:31:33 -0500

    started tooltip positioning

--- js/jquery.tinyTips.js
+++ js/jquery.tinyTips.js
@@ -1,46 +1,58 @@
 /***********************************************************/
 /*                    TinyTips Plugin                      */
-/*                      Version: 1.3                       */
+/*                       ersion: 1.3                       */
 /*                      Mike Merritt                       */
 /*         https://github.com/mikemerritt/TinyTips         */
 /***********************************************************/
 
 (function($) {
-  $.fn.tinyTips = function(options) {
-    
-    var defaults = {
-      content: 'title',
-      position: 'top'
-    };
-    
-    var options = $.extend(defaults, options);
-    var markup = '<div id="tinytip"></div>';
-    $('body').append(markup);
-    $('#tinytip').hide();
+	$.fn.tinyTips = function(options) {
+		
+		var defaults = {
+			content: 'title',
+			position: 'top'
+		};
+		
+		options = $.extend(defaults, options);
+		var markup = '<div id="tinytip"></div>';
+		$('body').append(markup);
+		var tip = $('#tinytip');
+		tip.hide();
 
-    $(this).on("mouseover", function() {
+		$(this).on("mouseover", function() {
 
-      console.log($(this).attr('tt'));
+			tip.append($(this).attr('tt'));
 
-      $('#tinytip').append($(this).attr('tt'));
+			var pos = $(this).position();
 
-      var pos = $(this).position();
+			var target = {
+				y: pos.top,
+				x: pos.left,
+				w: $(this).outerWidth(),
+				h: $(this).outerHeight()
+			};
 
-      var target = {
-        y: pos.top,
-        x: pos.left,
-        w: $(this).outerWidth(),
-        h: $(this).outerHeight(),
-        c: $(this).outerWidth()/2
-      };
+			var tt = {
+				top: {
+					y: target.y-5,
+					x: target.x-(tip.outerWidth()/2)+(target.w/2)
+				},
+				bot: {
+					y: target.y+tip.outerHeight()+target.h+5,
+					x: target.x-(tip.outerWidth()/2)+(target.w/2)
+				},
+				w: tip.outerWidth(),
+				h: tip.outerHeight()
+			};
 
-      var tooltip = {
-        top: {
-          y: target.y-20,
-          x: target.x
-        }
-      };
+			tip.show();
+			tip.css({top: tt.top.y-tt.h, left: tt.top.x});
 
-    });
-  }
+		});
+
+		$(this).on("mouseout", function() {
+			tip.hide();
+			tip.text("");
+		});
+	};
 })(jQuery);
